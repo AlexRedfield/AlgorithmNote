6aa39082a8a6f047162515faf77e86d712a244ff
alex <877428359@qq.com>
2019-01-21 23:56:04 +0800

    回文串

--- Project3/3.1.cpp
+++ Project3/3.1.cpp
@@ -4,9 +4,27 @@
 #include<cstring>
 using namespace std;
 
+//»ØÎÄ´®
+void cu5901() {
+	char c[260];
+	while (gets_s(c)) {
+		int len = strlen(c);
+		bool flag=true;
+		for (int i = 0; i < len / 2; i++) {
+			if (c[i] != c[len - i - 1])
+				flag = false;
+		}
+
+		if (flag)
+			printf("YES\n");
+		else
+			printf("NO\n");
+	}
+
+}
 
 int main() {
-	patb22();
+	cu5901();
 
 	system("pause");
 	return 0;
